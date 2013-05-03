07b4ec2e83f01ac681b4e1095a97326863e147c9
Mikael Brockman <mikael.brockman@gmail.com>
2013-05-04 01:54:18 +0200

    Log out link

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -13,4 +13,18 @@ feature 'Signing in' do
     click_button 'Sign in'
     page.should have_content("Signed in successfully.")
   end
+
+  scenario 'Sign out' do
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', :with => 'foo@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign in'
+
+    page.should_not have_link('Sign in')
+    page.should_not have_link('Sign up')
+
+    click_link 'Log out'
+    page.should have_link('Sign in')
+  end
 end
